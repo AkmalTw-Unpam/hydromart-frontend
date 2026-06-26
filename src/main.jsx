4e074a41f135dd082494e4507943284ef459ec64
AkmalTirtaWijaya <unpamakmal@gmail.com>
2026-06-27 00:23:46 +0700

    fix: temporary bypass service worker registration to clear asset cache

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('Service Worker terdaftar aman!', reg))
-      .catch(err => console.log('Service Worker gagal:', err));
-  });
-}
+//if ('serviceWorker' in navigator) {
+//  window.addEventListener('load', () => {
+//    navigator.serviceWorker.register('/sw.js')
+//      .then(reg => console.log('Service Worker terdaftar aman!', reg))
+//      .catch(err => console.log('Service Worker gagal:', err));
+//  });
+// }
